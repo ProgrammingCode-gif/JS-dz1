be3b5329af521dbc997631d739059760f04e5ca5
Shahzod <shahzodtashev49@gmail.com>
2022-05-09 21:44:27 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,22 @@
+var Name = prompt('Как вас зовут?');
+console.log('Вас зовут: ' + Name);
+
+var age = +prompt('Сколько вам лет?');
+console.log('Ваш возраст: ' + age);
+
+var division = +prompt('Решите пример: ' + 10 + '/' + 5);
+console.log('Правильный ответ: 2. Ваш ответ: ' + division);
+
+var multiplication = +prompt('Решите пример: ' + 10 + '*' + 5);
+console.log('Правильный ответ: ' + (10*5) + '. Ваш ответ: ' + multiplication);
+
+var addition = +prompt('Решите пример: ' + 10 + '+' + 5)
+console.log('Правильный ответ: ' + (10+5) + '. Ваш ответ: ' + addition);
+
+var subtraction = +prompt('Решите пример: ' + 10 + '-' + 5);
+console.log('Правильный ответ: ' + (10-5) + '. Ваш ответ: ' + subtraction);
+
+var percent = +prompt('Решите пример: ' + 10 + '%' + 3);
+console.log('Правильный ответ: ' + (10%3) + '. Ваш ответ: ' + subtraction);
+
+alert('Зайдите в консоль разработчика');
